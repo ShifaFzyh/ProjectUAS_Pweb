4146f8529994a69032cb7562e5c69cc083f69302
ShifaFzyh <shifafau05@gmail.com>
2025-01-13 11:14:29 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [
+  ],
+}
